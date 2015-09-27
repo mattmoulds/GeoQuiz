4fc8de379f34f0d60155cc47e42dd1f1bab71ba7
Matt <matthew.moulds1@gmail.com>
2015-09-27 10:12:16 +0100

    Added two toasts for the two anonymous inner classes

--- app/src/main/java/com/mattmoulds/geoquiz/QuizActivity.java
+++ app/src/main/java/com/mattmoulds/geoquiz/QuizActivity.java
@@ -6,6 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 
 public class QuizActivity extends ActionBarActivity {
@@ -22,14 +23,18 @@ public class QuizActivity extends ActionBarActivity {
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Does nothing yes, but soon!
+                Toast.makeText(QuizActivity.this,
+                        R.string.incorrect_toast,
+                        Toast.LENGTH_SHORT).show();
             }
         });
         mFalseButton = (Button)findViewById(R.id.false_button);
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Does nothing yes, but soon!
+                Toast.makeText(QuizActivity.this,
+                        R.string.correct_toast,
+                        Toast.LENGTH_SHORT).show();
             }
         });
     }
