a61bc66bf26d51f5ccd8dd443c5a8be798cfdbf4
Matt <matthew.moulds1@gmail.com>
2015-09-28 22:48:06 +0100

    Completed the challenge of adding a previous button. Added new button in XML and in the Quiz activity. Had an issue with the previous button when value is at index 0 but resolved

--- app/src/main/java/com/mattmoulds/geoquiz/QuizActivity.java
+++ app/src/main/java/com/mattmoulds/geoquiz/QuizActivity.java
@@ -16,6 +16,7 @@ public class QuizActivity extends ActionBarActivity {
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
+    private Button mPreviousButton;
     private TextView mQuestionTextView;
 
     private Question[] mQuestionBank = new Question[]{
@@ -88,7 +89,21 @@ public class QuizActivity extends ActionBarActivity {
         });
 
         updateQuestion();
+
+        mPreviousButton = (Button) findViewById(R.id.previous_button);
+        mPreviousButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mCurrentIndex == 0) mCurrentIndex = mQuestionBank.length -1;
+                else mCurrentIndex = mCurrentIndex -1;
+                updateQuestion();
+            }
+        });
+
+        updateQuestion();
     }
+
+
 
 
 
